d39559edbf2248765a6f69edf37ad71b91766756
Monique Elen <moniquelen10@hotmail.com>
2023-05-01 11:21:49 -0300

    adc cadastro de pets

--- divulgar/models.py
+++ divulgar/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Raca(models.Model):
     raca = models.CharField(max_length=50)
@@ -11,3 +12,21 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+
+class Pet(models.Model):
+    choices_status = (('P', 'Para adoção'),
+                      ('A', 'Adotado'))
+    
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    foto = models.ImageField(upload_to="fotos_pets")
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    estado = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    tags = models.ManyToManyField(Tag)
+    raca = models.ForeignKey(Raca, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=1, choices=choices_status, default='P')
+    
+    def __str__(self):
+        return self.nome
--- divulgar/views.py
+++ divulgar/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def novo_pet(request):
+    if request.method == "GET":
+        return render(request, 'novo_pet.html')
